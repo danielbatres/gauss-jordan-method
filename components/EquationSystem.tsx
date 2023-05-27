755248bbd66b5adb5b4a8155086f1f3e33bf37f2
danielbatres <danielbatresuser@gmail.com>
2023-05-27 15:05:24 -0600

    Fixed styles

--- components/EquationSystem.tsx
+++ components/EquationSystem.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import { MathDisplay } from './MathDisplay';
+import styles from '../styles/EquationSystem.module.sass'
 
 export const EquationSystem = ({ equations }): JSX.Element => {
-  const inlineStyles = {
-    fontSize: "1.4rem",
-    padding: "5px",
-    textAlign: "center"
-  }
-  
   return (
     <ul>
       {equations?.map((equation: string) => {
         return (
-          <li key={equation} style={inlineStyles}>
+          <li key={equation} className={styles.Style}>
               <MathDisplay math={equation} />
           </li>
         );
